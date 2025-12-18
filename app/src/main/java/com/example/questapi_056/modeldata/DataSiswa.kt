270ff4a84e6408af73e2c04790445c2c03a7af91
Dababah <pawasalee96@gamil.com>
2025-12-18 22:39:31 +0700

    Membuat Fungsi Ekstensi (Mapping)

--- app/src/main/java/com/example/questapi_056/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_056/modeldata/DataSiswa.kt
@@ -2,24 +2,23 @@ package com.example.questapi_056.modeldata
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class DataSiswa(
-    val id : Int,
-    val nama : String,
-    val alamat : String,
-    val telpon : String
+
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
 
-data class UIStateSiswa(
-    val detailSiswa: DetailSiswa = DetailSiswa(),
-    val isEntryValid: Boolean = false
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
 )
 
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = "",
-)
-
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
