871517410ed76563663f2b19696f12b440e4f0db
Dababah <pawasalee96@gamil.com>
2025-12-18 22:58:18 +0700

    Konek ke Internet (Implementasi)

--- app/src/main/java/com/example/questapi_056/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_056/repositori/RepositoryDataSiswa.kt
@@ -6,10 +6,8 @@ import com.example.questapi_056
 import com.example.questapi_056
 .modeldata.DataSiswa
 
-interface RepositoryDataSiswa{
-    suspend fun getDataSiswa() : List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    //suspend fun getStatusSiswa(id:Int) : DataSiswa
-    //suspend fun editStatusSiswa(id:Int,dataSiswa: DataSiswa) :retrofitr.Response<Void)
-    //suspend fun hapusStatusSiswa(id:Int) :retrofitr.Response<Void)
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+)
 }
