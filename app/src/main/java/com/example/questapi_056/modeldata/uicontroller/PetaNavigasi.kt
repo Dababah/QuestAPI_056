4ad17e519ea165ac81ab2864723b1cbc8b24b871
Dababah <pawasalee96@gamil.com>
2025-12-18 22:42:34 +0700

    ngatur Rute (Routing) Layar

--- app/src/main/java/com/example/questapi_056/modeldata/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_056/modeldata/uicontroller/PetaNavigasi.kt
@@ -13,10 +13,32 @@ import com.example.questapi_056.view.EntrySiswaScreen
 import com.example.questapi_056.view.HomeScreen
 
 @Composable
-fun DataSiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
 ){
-    HostNavigasi(navController = navController)
-}
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = {
+                   // navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
 
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
